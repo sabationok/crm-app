2a0ddb1e324825528fa1405a0253942241c78948
goodcat46 <110983329+sabationok@users.noreply.github.com>
2022-11-26 06:19:04 +0200

    4

--- src/components/Block/BlockActions/BlockActions.jsx
+++ src/components/Block/BlockActions/BlockActions.jsx
@@ -26,80 +26,81 @@ const BlockActions = ({
   }
   return (
     <>
-      <div className={s.actionsContainer}>
-        <ButtonIcon iconId={isOpen ? 'close' : 'actions-h'} size="100%" onClick={handleOpenActions} />
-      </div>
+      <ButtonIcon iconId={isOpen ? 'close' : 'actions-h'} size="100%" onClick={handleOpenActions} />
       <BlockPortal id={block.iconId}>
         <div className={isOpen ? s.actionsBackdropOpen : s.actionsBackdrop} onClick={handleOpenActions}>
-          {isOpen && (
-            <ul className={s.actionsList}>
-              {copy && (
-                <li className={s.listItem}>
-                  <ButtonIcon iconId="link" size="100%" className={s.hover} />
-                </li>
-              )}
-              {copy && (
-                <li className={s.listItem}>
-                  <ButtonIcon iconId="search" size="100%" className={s.hover} />
-                </li>
-              )}
-              {share && (
-                <li className={s.listItem}>
-                  <ButtonIcon iconId="filter-on" size="100%" className={s.hover} />
-                </li>
-              )}
-              {refresh && (
-                <li className={s.listItem}>
-                  <ButtonIcon iconId="copy" size="100%" className={s.hover} />
-                </li>
-              )}
-              {filter && (
-                <li className={s.listItem}>
-                  <ButtonIcon iconId="share" size="100%" className={s.hover} />
-                </li>
-              )}
-              {search && (
-                <li className={s.listItem}>
-                  <ButtonIcon iconId="refresh" size="100%" className={s.hover} />
-                </li>
-              )}
-              {editItem && (
-                <li className={s.listItem}>
-                  {' '}
-                  <ButtonIcon iconId="edit" size="100%" className={s.hover} />
-                </li>
-              )}
-              {deleteItem && (
-                <li className={s.listItem}>
-                  <ButtonIcon iconId="delete" size="100%" className={s.hover} />
-                </li>
-              )}
-              {createItem && (
-                <li className={s.listItem}>
-                  {' '}
-                  <ButtonIcon iconId="plus" size="100%" className={s.hover} />
-                </li>
-              )}
-              {printDoc && (
-                <li className={s.listItem}>
-                  {' '}
-                  <ButtonIcon iconId="print-on" size="100%" className={s.hover} />
-                </li>
-              )}
-              {exportDoc && (
-                <li className={s.listItem}>
-                  {' '}
-                  <ButtonIcon iconId="upload" size="100%" className={s.hover} />
-                </li>
-              )}
-              {importDoc && (
-                <li className={s.listItem}>
-                  {' '}
-                  <ButtonIcon iconId="download" size="100%" className={s.hover} />
-                </li>
-              )}
-            </ul>
-          )}
+          <div className={s.actionsContainer}>
+            <span>Додатково</span>
+            {isOpen && (
+              <ul className={s.actionsList}>
+                {link && (
+                  <li className={s.listItem}>
+                    <ButtonIcon iconId="link" size="100%" className={s.hover} />
+                  </li>
+                )}
+                {copy && (
+                  <li className={s.listItem}>
+                    <ButtonIcon iconId="search" size="100%" className={s.hover} />
+                  </li>
+                )}
+                {share && (
+                  <li className={s.listItem}>
+                    <ButtonIcon iconId="filter-on" size="100%" className={s.hover} />
+                  </li>
+                )}
+                {refresh && (
+                  <li className={s.listItem}>
+                    <ButtonIcon iconId="copy" size="100%" className={s.hover} />
+                  </li>
+                )}
+                {filter && (
+                  <li className={s.listItem}>
+                    <ButtonIcon iconId="share" size="100%" className={s.hover} />
+                  </li>
+                )}
+                {search && (
+                  <li className={s.listItem}>
+                    <ButtonIcon iconId="refresh" size="100%" className={s.hover} />
+                  </li>
+                )}
+                {editItem && (
+                  <li className={s.listItem}>
+                    {' '}
+                    <ButtonIcon iconId="edit" size="100%" className={s.hover} />
+                  </li>
+                )}
+                {deleteItem && (
+                  <li className={s.listItem}>
+                    <ButtonIcon iconId="delete" size="100%" className={s.hover} />
+                  </li>
+                )}
+                {createItem && (
+                  <li className={s.listItem}>
+                    {' '}
+                    <ButtonIcon iconId="plus" size="100%" className={s.hover} />
+                  </li>
+                )}
+                {printDoc && (
+                  <li className={s.listItem}>
+                    {' '}
+                    <ButtonIcon iconId="print-on" size="100%" className={s.hover} />
+                  </li>
+                )}
+                {exportDoc && (
+                  <li className={s.listItem}>
+                    {' '}
+                    <ButtonIcon iconId="upload" size="100%" className={s.hover} />
+                  </li>
+                )}
+                {importDoc && (
+                  <li className={s.listItem}>
+                    {' '}
+                    <ButtonIcon iconId="download" size="100%" className={s.hover} />
+                  </li>
+                )}
+              </ul>
+            )}
+          </div>
         </div>
       </BlockPortal>
     </>
