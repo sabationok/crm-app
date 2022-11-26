1e8275f764f8fa05245dfd78c4c8b60cfe0e10e9
sabationok <110983329+sabationok@users.noreply.github.com>
2022-11-26 04:04:12 +0000

    Deploying to gh-pages from @ sabationok/crm-app@06cc240b8d5b638bd8b82fdfb16c1446bcd8b888 🚀

---
+++ static/js/786.13fa0da9.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkcrm_app=self.webpackChunkcrm_app||[]).push([[786],{786:function(e,n,u){u.r(n),u.d(n,{default:function(){return r}});u(791);var a="PageLogOut_PageLogOut__91xaM",c=u(184),r=function(e){return(0,c.jsx)("div",{className:a,children:(0,c.jsx)("p",{children:"Goodbye!"})})}}}]);
+//# sourceMappingURL=786.13fa0da9.chunk.js.map
