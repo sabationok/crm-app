dd1e854bb00c7ce8dfd29b61d6d2d7c687dd565d
goodcat46 <110983329+sabationok@users.noreply.github.com>
2022-11-26 02:40:38 +0200

    1 basename router

--- src/index.js
+++ src/index.js
@@ -13,13 +13,13 @@ import './index.scss';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  // <React.StrictMode>
+  <React.StrictMode>
   <Provider store={store}>
     <PersistGate persistor={persistor}>
-      <BrowserRouter basename="/crm_test-react-">
+      <BrowserRouter basename="/crm-app">
         <App />
       </BrowserRouter>
     </PersistGate>
   </Provider>
-  // </React.StrictMode>
+ </React.StrictMode>
 );
