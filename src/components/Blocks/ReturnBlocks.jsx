20b95e5ef966d1e707bf1d8f0c12d0c055ff7277
goodcat46 <110983329+sabationok@users.noreply.github.com>
2022-12-04 15:01:59 +0200

    66

--- src/components/Blocks/ReturnBlocks.jsx
+++ src/components/Blocks/ReturnBlocks.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Block from 'components/Block/Block';
 import FormPrimary from 'components/Forms/FormPrimary/FormPrimary';
 export const BlockReturnsList = ({ tableTitles }) => {
-  return <Block title="Повернення" iconId="list" actions="withFilter" filterstyle={{ gridColumn: '1/11', gridRow: '1/5' }}></Block>;
+  return <Block title="Повернення" iconId="list" actions="withFilter" filter style={{ gridColumn: '1/11', gridRow: '1/5' }}></Block>;
 };
 export const BlockReturnInfo = () => {
   return (
