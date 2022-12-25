77dfde0804a1110d1564e1181834a9361913f31f
goodcat46 <107672708+goodcat46@users.noreply.github.com>
2022-12-25 17:14:22 +0200

    125
    + page ctx
    + blocks settings. start dev

--- src/data/blocksSettings.js
+++ src/data/blocksSettings.js
@@ -16,22 +16,22 @@ export const blocksNames = {
   BlockAdminSettings: 'BlockAdminSettings',
 };
 export const blocksSettings = [
-  { name: blocksNames.BlockProductsList, fullPageMode: true, title: 'Список продуктів' },
-  { name: blocksNames.BlockProductInfo, fullPageMode: true, title: 'Деталі замовлення' },
-  { name: blocksNames.BlockProductStock, fullPageMode: true, title: 'Склад' },
+  { name: blocksNames.BlockProductsList, fullPageMode: true, title: 'Список продуктів', iconId: 'list' },
+  { name: blocksNames.BlockProductInfo, fullPageMode: true, title: 'Деталі продукту', iconId: 'info' },
+  { name: blocksNames.BlockProductStock, fullPageMode: true, title: 'Склад', iconId: 'storage' },
 
-  { name: blocksNames.BlockOrdersList, fullPageMode: true, title: 'Список замовлень' },
-  { name: blocksNames.BlockOrderInfo, fullPageMode: true, title: 'Деталі замовлення' },
-  { name: blocksNames.BlockOrderRealizations, fullPageMode: true, title: 'Реалізації' },
-  { name: blocksNames.BlockOrderDeliveries, fullPageMode: true, title: 'Список відвантажень' },
+  { name: blocksNames.BlockOrdersList, fullPageMode: true, title: 'Список замовлень', iconId: 'list' },
+  { name: blocksNames.BlockOrderInfo, fullPageMode: true, title: 'Деталі замовлення', iconId: 'assignment-in' },
+  { name: blocksNames.BlockOrderRealizations, fullPageMode: true, title: 'Реалізації', iconId: 'assignment-ok' },
+  { name: blocksNames.BlockOrderDeliveries, fullPageMode: true, title: 'Список відвантажень', iconId: 'ttn' },
 
-  { name: blocksNames.BlockRefundsList, fullPageMode: true, title: 'Список повернень' },
-  { name: blocksNames.BlockRefundInfo, fullPageMode: true, title: 'Деталі повернень' },
-  { name: blocksNames.BlockRefundInspect, fullPageMode: true, title: 'Інспекція повернення' },
+  { name: blocksNames.BlockRefundsList, fullPageMode: true, title: 'Список повернень', iconId: 'list' },
+  { name: blocksNames.BlockRefundInfo, fullPageMode: true, title: 'Деталі повернень', iconId: 'assignment' },
+  { name: blocksNames.BlockRefundInspect, fullPageMode: true, title: 'Інспекція повернення', iconId: 'incpect' },
 
-  { name: blocksNames.BlockAdmin, fullPageMode: true, title: 'Адмін' },
-  { name: blocksNames.BlockManagers, fullPageMode: true, title: 'Менеджери' },
-  { name: blocksNames.BlockVendors, fullPageMode: true, title: 'Вендори' },
-  { name: blocksNames.BlockAdminRules, fullPageMode: true, title: 'Права доступу' },
-  { name: blocksNames.BlockAdminSettings, fullPageMode: true, title: 'Налаштування адмін панелі' },
+  { name: blocksNames.BlockAdmin, fullPageMode: true, title: 'Адмін', iconId: '' },
+  { name: blocksNames.BlockManagers, fullPageMode: true, title: 'Менеджери', iconId: '' },
+  { name: blocksNames.BlockVendors, fullPageMode: true, title: 'Вендори', iconId: '' },
+  { name: blocksNames.BlockAdminRules, fullPageMode: true, title: 'Права доступу', iconId: '' },
+  { name: blocksNames.BlockAdminSettings, fullPageMode: true, title: 'Налаштування адмін панелі', iconId: '' },
 ];
