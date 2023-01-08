096bcb16fb0164800397e85d013a92ab26f55fc4
goodcat46 <107672708+goodcat46@users.noreply.github.com>
2023-01-08 07:25:40 +0000

    Deploying to gh-pages from @ sabationok/crm-app@f11684ab45f6dfa58f2393a883ad274fec2b0ace 🚀

--- static/js/910.55cb9c52.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkcrm_app=self.webpackChunkcrm_app||[]).push([[910],{2910:function(s,e,n){n.r(e);var r=n(2829),c=n(4146),u=n(184);e.default=function(s){return(0,u.jsxs)(u.Fragment,{children:[" ",(0,u.jsxs)(r.t5,{children:[(0,u.jsx)(c.Hz,{}),(0,u.jsx)(c.DW,{}),(0,u.jsx)(c.Au,{})]}),(0,u.jsx)(r.gE,{children:(0,u.jsx)(c.Hz,{})})]})}}}]);
-//# sourceMappingURL=910.55cb9c52.chunk.js.map
